78a600a4c4ea0d1fc8414bdf9f612878c5298b92
daibv <152725726+daibv@users.noreply.github.com>
2023-12-08 00:14:29 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace sayhello
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your name:");//Chỉ viết thì ko cần WriteLine
+            string name = Console.ReadLine();//Sử dụng phương thức ReadLine() của lớp System.Console để nhập tên từ bàn phím. Gán thì ko cần Console
+            Console.WriteLine("Hello " + name + "!");//Sử dụng phương thức WriteLine() để hiển thị lời chào.
+            Console.Write("Very nice to meet you!");
+        }
+    }
+}
